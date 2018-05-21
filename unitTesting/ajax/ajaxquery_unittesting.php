4bdf595edad984bcb0417634ec77e65d95de908b
Onur Yukselen <onuryukselen@gmail.com>
2018-05-21 12:23:27 -0400

    17.05.2018_Version_0.1.36 travis fix

--- unitTesting/ajax/ajaxquery_unittesting.php
+++ unitTesting/ajax/ajaxquery_unittesting.php
@@ -1767,7 +1767,7 @@ class ajaxQueryTest extends TestCase
 		$_REQUEST['project_pipeline_id'] = '1';
 		$_REQUEST['id'] = '';
 		include('ajaxquery.php');
-		$this->assertEquals($data,null);
+		$this->assertEquals($data,[]);
 		ob_end_clean();
 	}
 	
